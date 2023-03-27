56b5b05b6bcf926c773eb42c5bbcf8456ae94653
vinicozzi <vinicozzimincent@gmail.com>
2023-03-27 16:57:10 -0400

    Build Move Tree

--- practice-for-ch-ruby-knights-travails-long-practice-main/knight.rb
+++ practice-for-ch-ruby-knights-travails-long-practice-main/knight.rb
@@ -8,10 +8,34 @@ class KnightPathFinder
         @considered_positions.add([5,1])
         @considered_positions.add(@pos)
         @root_node = PolyTreeNode.new(@pos)  # Start by creating an instance variable, `self.root_node` that stores the knight's initial position in an instance of your `PolyTreeNode` class.
-        # build_move_tree(root_node)# subject to change
+        build_move_tree # subject to change
     end
 
-    def build_move_tree(node) # the root node of the tree should be the knight's starting position
+    def build_move_tree
+
+        starting_moves = self.new_move_position(@root_node.pos) 
+        queue = [] 
+
+        starting_moves.each do |pos|
+            node = PolyTreeNode.new(pos) 
+            @root_node.add_child(node)
+            queue << node 
+        end
+        
+        until queue.empty? 
+            first_node = queue.shift 
+            first_node_moves = self.new_move_position(first_node.value)
+            first_node_moves.each do |move|
+                first_node.add_child(PolyTreeNode.new(move))
+
+            end
+
+            PolyTreeNode.add_child(first_pos)
+
+        end 
+
+
+        # the root node of the tree should be the knight's starting position
     end
 
     def self.valid_moves(pos) #assuming the pos is positive
@@ -54,7 +78,9 @@ class KnightPathFinder
 
         @considered_positions.merge(*unconsidered_positions)
         unconsidered_positions
-    end
+    end   
+
+    
 
 
 end
